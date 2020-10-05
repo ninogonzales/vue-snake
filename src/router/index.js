379ba66f611e4afb4502b2d4f2872da489189126
Nino Gonzales <ninogonzales@ninos-mbp.lan>
2020-10-04 17:31:32 -0700

    Configure routes

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,19 @@ const routes = [
     component: Menu
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/game",
+    name: "Game",
+    component: () => import("../views/Game.vue")
+  },
+  {
+    path: "/options",
+    name: "Options",
+    component: () => import("../views/Options.vue")
+  },
+  {
+    path: "/highscores",
+    name: "High Scores",
+    component: () => import("../views/HighScores.vue")
   }
 ];
 
